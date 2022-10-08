089dbea0379558e316b8798c470553490bb7f0f6
Luis Kain <luiskainlozano@gmail.com>
2022-10-08 07:08:40 -0500

    v1.5.0 se realizaron cambios sobre el método para seleccionar el valor de la carta, se eliminó el metodo y se remplazó con un arreglo.

--- src/Deck.java
+++ src/Deck.java
@@ -11,24 +11,24 @@ public class Deck {
     public Deck() {
     }
 
-    public void fill(){
+    public void fill(String[] valueToUse){
         for(int i = 0; i < 2; i++){
             for(int j = 0; j < 2; j++){
                 for(int k = 0; k < 13; k++){
                     switch (i){
                         case 0:
                             if(j == 0){
-                                myCards.add(new Card("Pica","Negro",selectValue(k)));
+                                myCards.add(new Card("Pica","Negro",valueToUse[k]));
 
                             }else {
-                                myCards.add(new Card("Trebol", "Negro", selectValue(k)));
+                                myCards.add(new Card("Trebol", "Negro", valueToUse[k]));
                             }
                             break;
                         case 1:
                             if (j == 0){
-                                myCards.add(new Card("Diamante","Rojo",selectValue(k)));
+                                myCards.add(new Card("Diamante","Rojo",valueToUse[k]));
                             }else{
-                                myCards.add(new Card("Corazon","Rojo",selectValue(k)));
+                                myCards.add(new Card("Corazon","Rojo",valueToUse[k]));
                             }
                             break;
                     }
@@ -36,54 +36,6 @@ public class Deck {
                 }
             }
         }
-    }
-
-    public String selectValue(int number){
-        String result = "";
-        switch (number){
-            case 0:
-                result = "2";
-                break;
-            case 1:
-                result = "3";
-                break;
-            case 2:
-                result = "4";
-                break;
-            case 3:
-                result = "5";
-                break;
-            case 4:
-                result = "6";
-                break;
-            case 5:
-                result = "7";
-                break;
-            case 6:
-                result = "8";
-                break;
-            case 7:
-                result = "9";
-                break;
-            case 8:
-                result = "10";
-                break;
-            case 9:
-                result = "A";
-                break;
-            case 10:
-                result = "J";
-                break;
-            case 11:
-                result = "Q";
-                break;
-            case 12:
-                result = "K";
-                break;
-        }
-        return result;
-
-
     }
 
     public void shuffle(){
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,27 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
+
 public class Main {
+    public static void showMenu(){
+        System.out.println("inserta el número correspondiente de la lista para usar el programa y su función " +
+                "segun el número que se muestra en la lista.");
+        System.out.println("Lista de opciones\n1 Mezclar deck\n2 Sacar una carta\n3 carta al azar" +
+                "\n4 Generar una mano de 5 cartas\n0 salir");
+    }
     public static void main(String[] args) {
         ArrayList<Card> myHand = new ArrayList<>();
         Deck myDeck = new Deck();
         Scanner sc =  new Scanner(System.in);
+        String[] myCardValues={"2","3","4","5","6","7","8","9","10","A","J","Q","K",};
         boolean isPlaying = true;
 
-        myDeck.fill();
+        myDeck.fill(myCardValues);
+        showMenu();
 
-        System.out.println("inserta el número correspondiente de la lista para usar el programa y su función" +
-                "segun el número que se muestra en la lista.");
-        System.out.println("Lista de opciones\n1 shuffle\n2 head\n3 pick\n4 hand\n5 exit");
 
         do{
-            System.out.printf("Inserte un número del 1 al 4 para elegír la función u otro para salir del programa:");
+            System.out.printf("Inserte un número del 1 al 4 para elegír la función o 0 para salir del programa:");
             switch (sc.nextInt()){
                 case 1:
                     myDeck.shuffle();
@@ -28,10 +35,12 @@ public class Main {
                 case 4:
                     myDeck.hand(myHand);
                     break;
-                default:
+                case 0:
                     isPlaying = false;
                     break;
-
+                default:
+                    System.out.println("opción invalida, solo números del 0 al 4");
+                    showMenu();
             }
 
         }while(isPlaying);
